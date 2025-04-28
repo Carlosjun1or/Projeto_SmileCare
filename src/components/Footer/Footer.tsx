1091c2062a9cef2f8e5d3b9f7c3ddafb179fc9fa
Carlos Roberto Dantas Junior <crdantasjr@gmail.com>
2025-04-28 20:44:22 -0300

    update

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+// components/Footer.tsx
+
+import "./Footer.css";
+export function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2025 Projeto Smile Care </p>
+    </footer>
+  );
+}
---
+++ src/components/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+// pages/Home.tsx
+
+import "./Home.css";
+export function Home() {
+  return (
+    <section className="page">
+      <h2 className="page-title">Página Inicial</h2>
+      <br />
+      <p>Bem-vindo ao nosso site!</p>
+      <br />
+      <img src="/sorisso.jpeg" alt="img" />
+    </section>
+  );
+}
