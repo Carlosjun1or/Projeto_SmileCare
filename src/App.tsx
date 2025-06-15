e7dec9544179b7d19ba562801bea00f0056075c4
Carlos Roberto Dantas Junior <crdantasjr@gmail.com>
2025-06-15 00:35:23 -0300

    teste

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./Routes";
-
-function App() {
-  return <RouterProvider router={router} />;
-}
-
-export default App;
